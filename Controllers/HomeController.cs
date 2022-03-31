a505847777aced8a2120d963670425014e0b5cc0
benjasch97 <schneider.benjamina97@gmail.com>
2022-03-30 23:49:09 -0600

    crud stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,19 +11,79 @@ namespace Mission13.Controllers
 {
     public class HomeController : Controller
     {
-        private BowlersDbContext _context { get; set; }
+        private IBowlersRepository _repo { get; set; }
 
         // Constructor
-        public HomeController(BowlersDbContext temp)
+        public HomeController(IBowlersRepository temp)
         {
-            _context = temp;
+            _repo = temp;
         }
 
         public IActionResult Index()
         {
-            var blah = _context.Bowlers.ToList();
+            var blah = _repo.Bowlers.ToList();
 
             return View(blah);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var x = new Bowler();
+
+            return View(x);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Bowler b)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Add(b);
+                _repo.Save(b);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+            var x = _repo.Bowlers.Single(x => x.BowlerID == bowlerid);
+            
+            return View("Edit", x);
+            
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler b, int bowlerid)
+        {
+            if (ModelState.IsValid)
+            {
+                Bowler oldbowler = _repo.Bowlers.Single(x => x.BowlerID == bowlerid);
+                _repo.Delete(oldbowler);
+                _repo.Add(b);
+                
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int bowlerid)
+        {
+            Bowler bowler = _repo.Bowlers.Single(x => x.BowlerID == bowlerid);
+            _repo.Delete(bowler);
+
+            return RedirectToAction("Index");
         }
 
     }
